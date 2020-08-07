f482e24e5cbec6d83e70cfd47847db772602ab01
markdiverta <marklee@diverta.co.jp>
2020-08-07 13:48:58 +0800

    Inquiry form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,6 +9,10 @@ const Heading = () => {
       <h3>Diverta Asia</h3>
       <p>Address: Unit 3A-05, Level 3A, Tower B, Vertical Business Suite, Bangsar South City, Jalan Kerinchi, 59200 Kuala Lumpur</p>
       <p>Tel: 03 7876-9161</p>
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input name="name" placeholder="Your Name" type="text"/>
+        <button>Send</button>
+      </form>
     </Layout>
   )
 }
